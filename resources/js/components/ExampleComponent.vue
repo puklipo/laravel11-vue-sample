ec704bda5ffebb21f2a1998f00f89d66cdd3187c
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-08-27 16:36:33 +0900

    Create ExampleComponent.vue

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {ref, onMounted} from 'vue'
+
+const user = ref();
+
+onMounted(async () => {
+    console.log('Component mounted.')
+
+    const res = await axios.get('/api/user');
+    user.value = await res.data;
+    console.log(user.value);
+})
+</script>
+
+<template>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <h2 class="text-2xl font-bold mb-3">Example Vue Component</h2>
+
+                    I'm an example component.
+
+                    <div>
+                        Name: {{ user?.name }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
