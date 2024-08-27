d8d9cfc2436a15bf87df5469144981eafa9a8fdd
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-08-27 16:36:39 +0900

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,12 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import { createApp } from 'vue';
+
+const app = createApp({});
+
+import ExampleComponent from './components/ExampleComponent.vue';
+app.component('example-component', ExampleComponent);
+
+app.mount('#app');
